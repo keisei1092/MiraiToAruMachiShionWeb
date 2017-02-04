96d9f971056a2e911292e2b1e5654519dadfc4cb
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-02-05 03:09:54 +0900

    [wip]ページきりかえ

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,32 @@
 var page = 0;
+var type = 'default';
+
+function $currentType() {
+  if (type == 'default') {
+    return $info;
+  } else if (type == 'news') {
+    return $news;
+  } else if (type == 'thoughts') {
+    return $thoughts;
+  }
+}
+
+function changeType(type) {
+  if (type == 'news') {
+    type = type == 'news' ? 'default' : 'news';
+  } else if (type == 'thoughts') {
+    type = type == 'thoughts' ? 'default' : 'thoughts';
+  }
+  page = 0;
+  repaint('down');
+}
 
 function nextInfo() {
-  if (page == $info.length - 1) {
+  if (page == $currentType().length - 1) {
     return;
   }
   page++;
-  repaint("down");
+  repaint('down');
 }
 
 function previousInfo() {
@@ -13,7 +34,7 @@ function previousInfo() {
     return;
   }
   page--;
-  repaint("up");
+  repaint('up');
 }
 
 function repaint(direction) {
@@ -22,7 +43,7 @@ function repaint(direction) {
   var transitionBefore;
   var transisionAfter;
 
-  if (direction == "up") {
+  if (direction == 'up') {
     transitionBefore = {
       'margin-top': '50px',
       'opacity': 0
@@ -46,10 +67,10 @@ function repaint(direction) {
     'duration': 750,
     'easing': 'swing',
     'complete': function() {
-      if (direction == "up") {
+      if (direction == 'up') {
         $('#main').css('margin-top', '-50px');
       }
-      $('#main').html($info[page]);
+      $('#main').html($currentType()[page]);
       $('#main').animate(transitionAfter, {
         'duration': 750,
         'easing': 'swing'
@@ -62,7 +83,7 @@ function repaint(direction) {
 $(document).on('ready', function() {
   $('body').hide(0).delay(500).fadeIn(500, function() {
     $('#main').hide();
-    $('#main').html($info[page]);
+    $('#main').html($currentType()[page]);
     $('#main').hide(0).delay(500).fadeIn(1000);
   });
 });
---
+++ public/scripts/special.js
@@ -0,0 +1,7 @@
+$(document).on('click', '#news', function() {
+  console.log('hoge');
+});
+
+$(document).on('click', '#thoughts', function() {
+  console.log('hoge');
+});
