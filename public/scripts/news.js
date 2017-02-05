041122c08122039357bcc5f09906dc64ecfe9678
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-02-05 13:34:02 +0900

    news

--- public/scripts/news.js
+++ public/scripts/news.js
@@ -1,5 +1,9 @@
+$second = $('<div></div>');
+
+$second.append($('<p></p>').text('2017/02/06 プレイヤー機能追加。わりとフツーになんとかなった。JavaScript力のたかまりを感じる'));
+
 $first = $('<div></div>');
 
 $first.append($('<p></p>').text('2017/02/05 ティザーサイト作成！'));
 
-$news = [$first];
+$news = [$second, $first];
