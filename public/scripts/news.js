45f2d5165af53ae847c416a6ca29974894c121f6
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-02-05 13:37:45 +0900

    改行

--- public/scripts/news.js
+++ public/scripts/news.js
@@ -1,9 +1,12 @@
 $second = $('<div></div>');
 
-$second.append($('<p></p>').text('2017/02/06 プレイヤー機能追加。わりとフツーになんとかなった。JavaScript力のたかまりを感じる'));
+$second.append($('<p></p>').text('2017/02/06'));
+$second.append($('<p></p>').text('プレイヤー機能追加。わりとフツーになんとかなった。'));
+$second.append($('<p></p>').text('JavaScript力のたかまりを感じる'));
 
 $first = $('<div></div>');
 
-$first.append($('<p></p>').text('2017/02/05 ティザーサイト作成！'));
+$first.append($('<p></p>').text('2017/02/05'));
+$first.append($('<p></p>').text('ティザーサイト作成！'));
 
 $news = [$second, $first];
